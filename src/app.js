09e895bbea53ee9386e0f1e503a44fabeb986ec6
brensmiranda <brendah.mirandaa@hotmail.com>
2022-07-23 14:08:39 -0300

    database :construction:

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+require('dotenv-safe').config();
+
+const db = require('./config/mongoConfig');
+///const rotasAlunos = require('./routes/AlunosRotas')
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+res.status(200).send({ 
+title: "Reprograma -> Fórmula Mágia da Paz - Projeto Final",
+version: "1.0.0",
+mensagem: "Bem-vindo ao meu projeto final!"
+
+
+})
+
+})
+
+db.connect();
+///app.use("/alunos", rotasAlunos);
+
+module.exports = app; 
